474588b4987f0bf752fd0c7eb909a89a25cc33f4
Xizafren <56082840+Xizafren@users.noreply.github.com>
2020-11-08 11:59:54 +0100

    Authentication that works but needs more tests, not the complete version

--- app/src/main/java/com/example/authentication/MainActivity.kt
+++ app/src/main/java/com/example/authentication/MainActivity.kt
@@ -1,15 +1,72 @@
 package com.example.authentication
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var auth: FirebaseAuth
+    lateinit var auth: FirebaseAuth
+ /*   private lateinit var buttonSignUp: Button
+    private lateinit var editTextEmail: EditText
+    private lateinit var editTextPassword: EditText
+    private lateinit var textViewResponse: TextView */
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        auth = Firebase.auth
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+      /*  val buttonSignUp: Button = findViewById(R.id.buttonSignUp)
+        val editTextPassword: EditText = findViewById(R.id.editTextPassword)
+        val editTextEmail: EditText = findViewById(R.id.editTextEmail)
+        val textViewResponse: TextView = findViewById(R.id.textViewResponse) */
+
+
+
+        buttonSignUp.setOnClickListener {
+        }
+ //       val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+//        inputMethodManager.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+            if (editTextEmail.text.toString().isNullOrEmpty() || editTextPassword.text.toString()
+                    .isNullOrEmpty()
+            )
+                textViewResponse.text = "Email Address or Password is not provided"
+            else {
+                auth.createUserWithEmailAndPassword(
+                    editTextEmail.text.toString(),
+                    editTextPassword.text.toString()
+                )
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            textViewResponse.text =
+                                "Sign Up successfull. Email and Password created"
+                            val user = auth.currentUser
+                            updateUI(user)
+                        } else {
+                            textViewResponse.text = "Sign Up Failed"
+                            updateUI(null)
+                        }
+                    }
+            }
+        }
+
+    private fun updateUI(currentUser: FirebaseUser?) {
+
     }
-}
+
+
+    }
+
+
